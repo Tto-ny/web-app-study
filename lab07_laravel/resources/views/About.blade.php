f74e8b07dacda1b2995f6379271a2b0a74ede6c2
Tto-ny <109040389+Tto-ny@users.noreply.github.com>
2025-08-25 20:04:51 +0700

    commit lab7

---
+++ lab07_laravel/resources/views/About.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><b>เกี่ยวกับผู้จัดทํา</b></h1><p></p>
+    <b>ชื่อ-นามสกุล: </b>วรพันธุ์ คำทะเนตร<p></p>
+    <b>รหัสนักศึกษา: </b>663380534-6<p></p>
+    <b>หลักสูตร: </b>ปัญญาประดิษฐ์<p></p>
+    <b>มหาวิทยาลัย: </b>มหาวิทยาลัยขอนแก่น<p></p>
+    <b>อีเมล: </b>woraphan.k@kkumail.com
+</body>
+</html>
---
+++ lab07_laravel/resources/views/movies.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+    <a href="/">Home</a> |
+    <a href="/about">About</a> |
+    <a href="/movie">Movie Lists</a><hr>
+    <h2>รายชื่อภาพยนตร์</h2>
+    <table >
+        <tr>
+            <th >ลำดับ</th>  
+            <th >ชื่อภาพยนตร์</th>  
+            <th >ผู้กำกับ</th>  
+            <th >ปีที่ฉาย</th>  
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Inception</td>
+            <td>Christopher Nolan</td>
+            <td>2010</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Parasite</td>
+            <td>Bong joon-ho</td>
+            <td>2019</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Spirited Away</td>
+            <td>Hayao Miyazaki</td>
+            <td>2001</td>
+        </tr>
+    </table>
+</body>
+</html>
