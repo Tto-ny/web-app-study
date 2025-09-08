59908c26142cc9e216a27794ae52b572de91bd7b
Tto-ny <109040389+Tto-ny@users.noreply.github.com>
2025-09-08 14:26:07 +0700

    new commit lab 6

--- lab06/insert_book.php
+++ lab06/insert_book.php
@@ -1,35 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
 
-    <?php
-        echo "<h3>เพิ่มหนังสือเรียบร้อยแล้ว</h3>";
-        echo "<h2>รายการหนังสือในห้องสมุด</h2>";
-    ?>
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $title = $_POST['title'];
-    $author = $_POST['author'];
-    $year = $_POST['year'];
-    $category = $_POST['category'];
-
-    $conn = mysqli_connect("localhost", "root", "", "ai_lab06");
-    mysqli_set_charset($conn, "utf8");
-
-    $stmt = $conn->prepare("INSERT INTO books (title, author, year, category) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssis", $title, $author, $year, $category);
-    $stmt->execute();
-    $stmt->close();
-
-    $sql = "SELECT * FROM books";
-    $result = mysqli_query($conn, $sql);
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $dbname = "ai_lab06";
+    $conn = mysqli_connect($host, $user, $password, $dbname);
+    if($conn){
+        if(isset($_POST["title"]) && isset($_POST["author"]) && isset($_POST["year"])){
+            $title = $_POST["title"];
+            $author = $_POST["author"];
+            $year = $_POST["year"];
+            $category = $_POST["category"];
+            $sql = "INSERT INTO books(title, author, year,category) VALUES ('$title', '$author', $year,'$category')";
+                if(mysqli_query($conn, $sql)){
+                    $last_id = mysqli_insert_id($conn);
+                    echo "<h3>เพิ่มหนังสือเรียบร้อยแล้ว</h3>";}
+                else{
+                    echo "ไม่สามารถบันทึกข้อมูลได้<br>";
+                }
+                $sql = "SELECT * FROM books";
+                $result = mysqli_query($conn, $sql);
+            }
+        else{
+            echo "ไม่สามารถเชื่อมต่อกับฐานข้อมูลได้";
+            exit;
+        }}
+            ?>
+    <?php
+        echo "<h2>รายการหนังสือในห้องสมุด</h2>";
 
     if (mysqli_num_rows($result) > 0) {
         echo "<table border='1' cellpadding='10'>";
@@ -50,9 +56,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     mysqli_close($conn);
 
     echo "<br><a href='book_form.html'>เพิ่มหนังสือ</a>";
-}
 ?>
-
 </body>
-
 </html>
